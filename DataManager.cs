5fc63c394c6c350d42aeef23d18e5338dbfa9d0e
Spythere <spythere@protonmail.com>
2021-12-06 16:08:29 +0100

    Add project files.

---
+++ DataManager.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ANP_Helper
+
+{
+     public class StationOnline
+    {
+        public int dispatcherId { get; set; }
+        public string dispatcherName { get; set; }
+        public bool dispatcherIsSupporter { get; set; }
+        public string stationName { get; set; }
+        public string stationHash { get; set; }
+        public string region { get; set; }
+        public int maxUsers { get; set; }
+        public int currentUsers { get; set; }
+        public int spawn { get; set; }
+        public object lastSeen { get; set; }
+        public int dispatcherExp { get; set; }
+        public string nameFromHeader { get; set; }
+        public string spawnString { get; set; }
+        public string networkConnectionString { get; set; }
+        public int isOnline { get; set; }
+        public int dispatcherRate { get; set; }
+    }
+
+    public class StationAPIResponse
+    {
+        public bool success { get; set; }
+        public int respCode { get; set; }
+        public List<StationOnline> message { get; set; }
+    }
+    class TrainAPIResponse
+    {
+        public bool success { get; set; }
+        public int respCode { get; set; }
+        public List<TrainOnline> message { get; set; }
+    }
+
+    class TrainInfo
+    {
+        public int timetableId { get; set; }
+        public int trainNo { get; set; }
+        public string trainCategoryCode { get; set; }
+        public int driverId { get; set; }
+        public string driverName { get; set; }
+        public string route { get; set; }
+        public int twr { get; set; }
+        public int skr { get; set; }
+        public List<string> sceneries { get; set; }
+    }
+
+    class StopPoint
+    {
+        public string arrivalLine { get; set; }
+        public DateTime? arrivalTime { get; set; }
+        public int arrivalDelay { get; set; }
+        public DateTime? arrivalRealTime { get; set; }
+        public double pointDistance { get; set; }
+        public string pointName { get; set; }
+        public string pointNameRAW { get; set; }
+        public int entryId { get; set; }
+        public string pointId { get; set; }
+        public object comments { get; set; }
+        public int confirmed { get; set; }
+        public int isStopped { get; set; }
+        public int? pointStopTime { get; set; }
+        public string pointStopType { get; set; }
+        public string departureLine { get; set; }
+        public DateTime? departureTime { get; set; }
+        public int departureDelay { get; set; }
+        public DateTime? departureRealTime { get; set; }
+    }
+
+    class Message
+    {
+        public TrainInfo? trainInfo { get; set; }
+        public List<StopPoint>? stopPoints { get; set; }
+    }
+
+    class TimetableAPIResponse
+    {
+        public bool success { get; set; }
+        public int respCode { get; set; }
+        public Message? message { get; set; }
+    }
+
+    class TrainOnline
+    {
+        public int trainNo { get; set; }
+        public int driverId { get; set; }
+        public string driverName { get; set; }
+
+        public string region { get; set; }
+    }
+
+    class Timetable
+    {
+        public TrainInfo trainInfo { get; set; }
+        public List<StopPoint> stopPoints { get; set; }
+    }
+
+    class SceneryTimetable
+    {
+        public TrainInfo trainInfo { get; set; }
+    
+        public StopPoint sceneryStopPoint { get; set; }
+    }
+
+    class DataManager
+    {
+        readonly HttpClient client = new HttpClient();
+        readonly string chosenScenery = "drzewko";
+
+
+        public DataManager(string chosenScenery)
+        {
+            this.chosenScenery = chosenScenery;
+        }
+
+        public async Task<bool> isChosenSceneryOnline()
+        {
+            string responseString = await client.GetStringAsync("https://api.td2.info.pl:9640/?method=getStationsOnline");
+            StationAPIResponse response = JsonConvert.DeserializeObject<StationAPIResponse>(responseString);
+            List<StationOnline> onlineStations = response.message;
+
+            return onlineStations.Find(station => station.nameFromHeader.ToLower().Equals(chosenScenery.ToLower())) != null;
+        }
+
+        public async Task<List<SceneryTimetable>> fetchTimetableData()
+        {
+            Trace.WriteLine("Ładowanie danych...");
+            string responseString = await client.GetStringAsync("https://api.td2.info.pl:9640/?method=getTrainsOnline");
+
+            TrainAPIResponse response = JsonConvert.DeserializeObject<TrainAPIResponse>(responseString);
+
+            List<TrainOnline> onlineTrains = response.message;
+
+            List<Task<TimetableAPIResponse>> tasks = new List<Task<TimetableAPIResponse>>();
+
+            Parallel.ForEach(onlineTrains, train =>
+            {
+                if (train.region != "eu")
+                    return;
+
+                Task<TimetableAPIResponse> task = getTimetableData(client, $"https://api.td2.info.pl:9640/?method=readFromSWDR&value=getTimetable%3B{train.trainNo}%3Beu");
+
+                tasks.Add(task);
+            });
+
+            TimetableAPIResponse[] ttResponse = await Task.WhenAll(tasks);
+
+            Trace.WriteLine("Załadowano!");
+
+            List<SceneryTimetable> sceneryTimetables = new List<SceneryTimetable>();
+
+            foreach (TimetableAPIResponse res in ttResponse)
+            {
+                if (res == null)
+                    continue;
+
+                if (res.message.trainInfo == null)
+                    continue;
+
+
+                StopPoint sceneryStopPoint = res.message.stopPoints.Find(stop => stop.pointNameRAW.ToLower().Equals(chosenScenery.ToLower()));
+
+
+                if (sceneryStopPoint == null)
+                    continue;
+
+                sceneryTimetables.Add(new SceneryTimetable
+                {
+                    trainInfo = res.message.trainInfo,
+                    sceneryStopPoint = sceneryStopPoint
+                });
+            }
+
+            sceneryTimetables.Sort((a, b) => a.trainInfo.timetableId > b.trainInfo.timetableId ? 1 : -1);
+
+            return sceneryTimetables;
+        }
+
+
+        private async Task<TimetableAPIResponse> getTimetableData(HttpClient client, string url)
+        {
+            string res = await client.GetStringAsync(url);
+
+            TimetableAPIResponse resp;
+            try
+            {
+                resp = JsonConvert.DeserializeObject<TimetableAPIResponse>(res);
+            } catch (JsonSerializationException)
+            {
+                Trace.WriteLine("Ups! Błąd przy " + url);
+                return null;
+            }
+
+            return resp;
+        } 
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using System.Windows.Threading;
+
+namespace ANP_Helper
+{
+    public class TimetableColumn
+    {
+        public int timetableId { get; set; }
+        public int trainNo { get; set; }
+        public string driverName { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        readonly DataManager dm;
+        string chosenScenery = "Piaskowo";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            statusTextXAML.Text = $"{chosenScenery.ToUpper()} OFFLINE!";
+
+
+            dm = new DataManager(chosenScenery);
+
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(10)
+            };
+
+            timer.Tick += timerTick;
+            timer.Start();
+
+            getSceneryData();
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            getSceneryData();
+        }
+
+        public async void getSceneryData()
+        {
+            bool isSceneryOnline = await dm.isChosenSceneryOnline();
+
+            if(!isSceneryOnline)
+                return;
+
+            statusIndicatorXAML.Fill = Brushes.LightGreen;
+
+            statusTextXAML.Text = $"{chosenScenery.ToUpper()} ONLINE!";
+            statusTextXAML.Foreground = Brushes.LightGreen;
+
+            List<SceneryTimetable> timetables = await dm.fetchTimetableData();
+
+            List<TimetableColumn> columns = new List<TimetableColumn>();
+
+            foreach (SceneryTimetable timetable in timetables)
+            {
+                columns.Add(new TimetableColumn
+                {
+                    timetableId = timetable.trainInfo.timetableId,
+                    trainNo = timetable.trainInfo.trainNo,
+                    driverName = timetable.trainInfo.driverName
+                });
+            }
+
+            DataGridXAML.ItemsSource = columns;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGridXAML_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void DataGridXAML_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
